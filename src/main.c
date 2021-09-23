d28cca1863ce313740c0b3c7361904dd9cd6f951
Guillaume GILBERT <ggilbert@e2r9p15.clusters.42paris.fr>
2021-09-23 15:25:21 +0200

    cf print_msg

--- src/main.c
+++ src/main.c
@@ -6,18 +6,28 @@
 /*   By: ggilbert <ggilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 17:44:52 by ggilbert          #+#    #+#             */
-/*   Updated: 2021/09/22 19:02:41 by ggilbert         ###   ########.fr       */
+/*   Updated: 2021/09/23 14:36:49 by ggilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <string.h>
 
 void	print_msg(char *msg, t_philo *philo)
 {
+	int die;
+
+	die = strcmp(msg, MSG_DIED);
+	printf("diiiiie %d\n", die);
+	//le probleme est que si il meurt il ne peut pas le print ==> loop
+	pthread_mutex_lock(philo->mutex_all_alive);
+	if (*philo->all_alive == -1 && !die)
+		return ;
 	pthread_mutex_lock(philo->mutex_talk);
 	printf("%6ld %d %s\n", get_millis() - philo->time_start, philo->id + 1, msg);
 	pthread_mutex_unlock(philo->mutex_talk);
-}
+	pthread_mutex_unlock(philo->mutex_all_alive);
+	}
 
 int	main(int ac, char **av)
 {
